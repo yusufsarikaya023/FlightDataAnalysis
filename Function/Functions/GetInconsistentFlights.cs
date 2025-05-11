8faeca0c8d8eb5f7e721f8e55cb993c77ac7c18c
yusufsarikaya023 <yusufsarikaya023@gmail.com>
2025-05-11 20:47:18 +0300

    Refactor GetInconsistentFlights class to remove logger dependency; add functional tests for GetInconsistentFlights functionality

--- Function/Functions/GetInconsistentFlights.cs
+++ Function/Functions/GetInconsistentFlights.cs
@@ -4,14 +4,13 @@ using Application.UseCases.FlightData.DTO;
 
 namespace Function.Functions;
 
-public class GetInconsistentFlights(ILogger<GetFlights> logger, IMediator mediator)
+public class GetInconsistentFlights(IMediator mediator)
 {
     [Function("GetInconsistentFlights")]
     [OpenApiOperation("GetInconsistentFlights")]
     [OpenApiResponseWithBody(HttpStatusCode.OK, "application/json", typeof(FlightDto[]))]
     public async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Function, "post")] HttpRequest req)
     {
-        logger.LogInformation("C# HTTP trigger function processed a request.");
         var result = await mediator.Send(new GetInconsistentFlightsQuery());
         return new OkObjectResult(result);
     }
---
+++ Test/FunctionalTest/Functions/GetInconsistentFlightsTest.cs
@@ -0,0 +1,54 @@
+using System.Text;
+using Application.Common;
+using Application.UseCases.FlightData.DTO;
+using Domain.Aggregation.Flights;
+using Function.Functions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json;
+
+namespace Test.FunctionalTest.Functions;
+
+[Collection("FunctionalCollection")]
+public class GetInconsistentFlightsTest(FunctionFixture fixture)
+{
+    private readonly GetInconsistentFlights _sut = new(fixture.Host.Services.GetRequiredService<IMediator>());
+    
+    [Fact]
+    public async Task Get_Flights_Should_Return_200()
+    {
+        // arrange
+        var departureAirport = Faker.GetFakeAirport();
+        var arrivalAirport = Faker.GetFakeAirport();
+        fixture.Context.Airports.AddRange(departureAirport, arrivalAirport);
+        
+        var airCraft = Faker.GetFakeAirCraft();
+        fixture.Context.AirCrafts.Add(airCraft);
+        // Save changes to get the Ids
+        await fixture.Context.SaveChangesAsync(CancellationToken.None);
+        
+        var flight = Faker.GetFakeFlight();
+        flight.SetDepartureAirportId(departureAirport.Id)
+            .SetArrivalAirportId(arrivalAirport.Id)
+            .SetAirCraftId(airCraft.Id)
+            .SetConsistencyType(FlightConsistencyType.Inconsistent)
+            .SetInconsistencyReason("Test reason");
+        
+        fixture.Context.Add(flight);
+        await fixture.Context.SaveChangesAsync(CancellationToken.None);
+        var body = new Pagination(1, 10);
+        var httpRequest = new Mock<HttpRequest>();
+        httpRequest.Setup(r => r.Body)
+            .Returns(new MemoryStream(Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(body))));
+                                                        
+        // act
+        var result = await _sut.Run(httpRequest.Object);
+        var flights = (result as OkObjectResult)?.Value as FlightDto[];
+
+        // assert
+        Assert.Equal(200, (result as OkObjectResult)?.StatusCode);
+        Assert.NotNull(flights);
+        Assert.NotEmpty(flights);
+    }
+}
